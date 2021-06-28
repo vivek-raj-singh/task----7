2e3f656451d59c56a1757b7625fad68ce51261a3
vivek-raj-singh <84335962+vivek-raj-singh@users.noreply.github.com>
2021-06-28 23:50:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var start = document.getElementById('start');
 var reset = document.getElementById('reset');
-
+var t=0;
 var h = document.getElementById("hour");
 var m = document.getElementById("minute");
 var s = document.getElementById("sec");
@@ -22,18 +22,27 @@ reset.addEventListener('click', function(){
     h.value = 0;
     m.value = 0;
     s.value = 0;
+    t=0;
     //stop the timer after pressing "reset"
     stopInterval()
 })
 
 function timer(){
     if(h.value == 0 && m.value == 0 && s.value == 0){
+       if(t==0)
         alert("time is up");
         h.value = 00;
         m.value = 00;
         s.value = 00;
-        
+        t=1;
+        h.value = 0;
+        m.value = 0;
+        s.value = 0;
+        t=0;
+        //stop the timer after pressing "reset"
+        stopInterval()
     }
+    else
     if(h.value == 0 && m.value == 0 && s.value == 0){
         
         h.value = 0;
@@ -49,9 +58,9 @@ function timer(){
         m.value = 60;
         h.value--;
     }
-    else if(h.value == 0 && m.value == 0 && s.value == 0){
-        alert("time is up");
-    }
+    // else if(h.value == 0 && m.value == 0 && s.value == 0){
+    //      alert("time is up");
+    // }
     return;
 }
 
